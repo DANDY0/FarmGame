483054015737e10303a24c67c963e79297de2e83
DANDY\nesin <nesindan@gmail.com>
2025-11-16 15:29:14 +0200

    wip: part 6 done

--- Assets/Scripts/FarmManager.cs
+++ Assets/Scripts/FarmManager.cs
@@ -14,6 +14,13 @@ public class FarmManager : MonoBehaviour
     public Color buyColor = Color.green;
     public Color cancelColor = Color.red;
 
+    public bool isSelecting = false;
+    public int selectedTool=0;
+    // 1- water 2- Fertilizer 3- Buy plot
+
+    public Image[] buttonsImg;
+    public Sprite normalButton;
+    public Sprite selectedButton;
 
     // Start is called before the first frame update
     void Start()
@@ -25,23 +32,56 @@ public class FarmManager : MonoBehaviour
     {
         if(selectPlant == newPlant)
         {
-            selectPlant.btnImage.color = buyColor;
-            selectPlant.btnTxt.text = "Buy";
-            selectPlant = null;
-            isPlanting = false;
+            CheckSelection();
             
         }
         else
         {
-            if (selectPlant != null)
-            {
-                selectPlant.btnImage.color = buyColor;
-                selectPlant.btnTxt.text = "Buy";
-            }
+            CheckSelection();
             selectPlant = newPlant;
             selectPlant.btnImage.color = cancelColor;
             selectPlant.btnTxt.text = "Cancel";
             isPlanting = true;
+        }
+    }
+
+    public void SelectTool(int toolNumber)
+    {
+        if(toolNumber == selectedTool)
+        {
+            //deselect
+            CheckSelection();
+        }
+        else
+        {
+            //select tool number and check to see if anything was also selected
+            CheckSelection();
+            isSelecting = true;
+            selectedTool = toolNumber;
+            buttonsImg[toolNumber - 1].sprite = selectedButton;
+        }
+    }
+
+    void CheckSelection()
+    {
+        if (isPlanting)
+        {
+            isPlanting = false;
+            if (selectPlant != null)
+            {
+                selectPlant.btnImage.color = buyColor;
+                selectPlant.btnTxt.text = "Buy";
+                selectPlant = null;
+            }
+        }
+        if (isSelecting)
+        {
+            if (selectedTool > 0)
+            {
+                buttonsImg[selectedTool - 1].sprite = normalButton;
+            }
+            isSelecting = false;
+            selectedTool = 0;
         }
     }
 
